569718436a603e5ea1d517991d2cca95d8ce6dbc
Alan Bushell <alanbushell91@gmail.com>
2022-12-14 11:28:52 +0000

    add product model and format on admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = (
+        'friendly_name',
+        'name',
+        'slug',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'category',
+                    'last_modified', 'in_stock')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     friendly_name = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(max_length=250, blank=True)
 
@@ -17,4 +17,27 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = 'category'
-        verbose_name_plural = 'categories'
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField()
+    in_stock = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    item_added = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.product_name
+
+    def check_stock(self):
+        if stock >= 1:
+            in_stock = True
+        else:
+            in_stock = False
