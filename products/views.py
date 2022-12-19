281b550c251cdaaf60d8ab7965be615771777ed2
Alan Bushell <alanbushell91@gmail.com>
2022-12-19 05:14:36 +0000

    remove fixed nav & margin affecting layout

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
 
@@ -12,3 +12,15 @@ def products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
