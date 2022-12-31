b24b9eacfad247f3b651002807029a6fac740693
Alan Bushell <alanbushell91@gmail.com>
2022-12-31 04:21:40 +0000

    restart cart app and add product to cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -22,3 +22,18 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """ Adjust quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
--- products/models.py
+++ products/models.py
@@ -31,6 +31,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField()
     in_stock = models.BooleanField(default=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     item_added = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
