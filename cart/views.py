00ceead0bfecd7b8038f41a7816680874ac56365
Alan Bushell <alanbushell91@gmail.com>
2022-12-31 07:38:24 +0000

    add remove function for cart products, fix subtotal bug

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -41,12 +41,44 @@ def adjust_cart(request, item_id):
     """ Adjust quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     cart = request.session.get('cart', {})
 
-    if quantity > 0:
-        cart[item_id] = quantity
-    else:
-        cart.pop[item_id]
+    if size:  # If the items have sizes run this
+        if quantity > 0:  # if item is alreadyin basket run this
+            cart[item_id]['items_by_size'] = quantity
+        else:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+    else:  # if no sizes run this
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """ Adjust quantity of the specified product to the shopping bag """
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        cart = request.session.get('cart', {})
+
+        if size:  # If the items have sizes run this
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop[item_id]
+        else:  # if no sizes run this
+            cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
