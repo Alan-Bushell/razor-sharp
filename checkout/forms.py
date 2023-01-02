f8643b2dfc78330afce7b58eb8a1faadb13b0b08
Alan Bushell <alanbushell91@gmail.com>
2023-01-02 07:48:17 +0000

    add stripe payment to checout flow

--- checkout/forms.py
+++ checkout/forms.py
@@ -32,9 +32,9 @@ class OrderForm(forms.ModelForm):
         self.fields['first_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
-                placeholder = f'{placeholders[field]}'
+                placeholder = f'{placeholders[field]} *'
             else:
                 placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].widget.label = False
+            self.fields[field].label = False
