696dce9ac53b003ea153a0fc1593284d306af438
Alan Bushell <alanbushell91@gmail.com>
2023-01-03 11:02:01 +0000

    add base template and layout to profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.accounts, name='accounts'),
+    path('shipping_details/', views.shipping_details, name='shipping_details'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def accounts(request):
     """ A view to return profile page """
     return render(request, 'accounts/account.html')
+
+def shipping_details(request):
+    """ A view to return profile page """
+    return render(request, 'accounts/shipping_details.html')
