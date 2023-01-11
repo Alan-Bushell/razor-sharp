3a149f8213b8d186abc8431ad639cb23274ec6a6
Alan Bushell <alanbushell91@gmail.com>
2023-01-11 08:26:25 +0000

    added user forms to allow for front end updates

---
+++ accounts/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from datetime import datetime
+from django.urls import reverse_lazy
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from .models import User, Account, UserProfile
+from django.forms import ModelForm
+from django_countries.fields import CountryField
+
+
+# Profile account forms
+
+class AccountForm(ModelForm):
+    """User account form to edit base information """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+    first_name = forms.CharField(max_length=40)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField(required=True)
+    phone = forms.CharField(max_length=15)
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'email', 'phone']
+
+
+class ImageForm(ModelForm):
+    """Image form to update profile picture"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture']
+
+
+class AddressForm(ModelForm):
+    """Address form to change shipping information"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+    street_address1 = forms.CharField(max_length=100)
+    street_address2 = forms.CharField(max_length=100)
+    town_or_city = forms.CharField(max_length=30)
+    county = forms.CharField(max_length=30)
+    postcode = forms.CharField(max_length=30)
+    country = CountryField(max_length=5)
+
+    class Meta:
+        model = UserProfile
+        fields = ['street_address1', 'street_address2', 'town_or_city',
+                  'county', 'postcode', 'country']
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from checkout.models import Order
 from .models import UserProfile, Account
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from .forms import AccountForm
 
 
 # Create your views here.
@@ -33,10 +34,20 @@ def order_history(request):
 
 def edit_profile(request, user_id):
     userprofile = get_object_or_404(UserProfile, user=user_id)
-
-    template = 'accounts/edit_profile.html'
-
-    return render(request, template)
+    form = AccountForm(instance=userprofile)
+    if request.POST:
+        form = AccountForm(request.POST, instance=userprofile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             'Your profile has been updated')
+        return redirect('profile')
+    userprofile = AccountForm(instance=userprofile)
+    context = {
+        'form': form
+    }
+    template = 'edit_profile.html'
+    return render(request, template, context)
 
 
 # Signal used to create user profile once user created
