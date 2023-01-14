d480d21379495089c19726bfc2abdab429eaa615
Alan Bushell <alanbushell91@gmail.com>
2023-01-14 15:36:00 +0000

    fix bug in settings.py related to s3

--- razor_sharp/settings.py
+++ razor_sharp/settings.py
@@ -189,7 +189,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY_ID')
-    AWS_S3_CUSTOM_DOMAIN = f'{ANS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     #  Static & Media Storage
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
