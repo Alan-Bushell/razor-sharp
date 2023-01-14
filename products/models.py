c689cf86cb2601f2ba72f923dbd159e38e8f8da8
Alan Bushell <alanbushell91@gmail.com>
2023-01-14 06:25:14 +0000

    add rating logic to model and product front end.

--- products/models.py
+++ products/models.py
@@ -38,6 +38,7 @@ class Product(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    rating = models.FloatField(null=True)
 
     def __str__(self):
         return self.product_name
