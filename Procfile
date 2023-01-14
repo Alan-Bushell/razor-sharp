78269293a0983eff41784dfc54318ae7196b528e
Alan Bushell <alanbushell91@gmail.com>
2023-01-14 13:33:51 +0000

    connected to hheroku, elephantsql

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn razor_sharp:wsgi:application
--- razor_sharp/settings.py
+++ razor_sharp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-+v)ry3keiuq$nqlf885%s)j@nva1=0vunen1p*0ewumkkoz7l@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abushell-razor-sharp.heroku.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +127,17 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
