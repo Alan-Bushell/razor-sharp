d4db78e245813ee849a9a6ccd642530f72a9aca7
Alan Bushell <alanbushell91@gmail.com>
2023-01-14 13:42:20 +0000

    remove secret key

--- razor_sharp/settings.py
+++ razor_sharp/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+v)ry3keiuq$nqlf885%s)j@nva1=0vunen1p*0ewumkkoz7l@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['abushell-razor-sharp.heroku.com', 'localhost']
 
