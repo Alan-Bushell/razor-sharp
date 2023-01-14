9052c0ead2893c0b2467ae8ed3bf073c7ee91127
Alan Bushell <alanbushell91@gmail.com>
2023-01-14 15:51:53 +0000

    fix bug in custom_storages.py - typo

--- custom_storages.py
+++ custom_storages.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from storages.backends.s3boto import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
+
 
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
