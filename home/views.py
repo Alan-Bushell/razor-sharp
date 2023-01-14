93637c322c608bf701399ef5be5e2604e594183e
Alan Bushell <alanbushell91@gmail.com>
2023-01-14 16:44:24 +0000

    remove 500page

--- home/views.py
+++ home/views.py
@@ -14,9 +14,3 @@ def handle_404_error(request, exception):
     """View to return 404 if page not found"""
     template = 'home/templates/home/404.html'
     return render(request, template, {})
-
-
-def handle_500_error(request):
-    """View to return 500 if page not found"""
-    template = 'home/templates/home/500.html'
-    return render(request, template, {})
