881657597cda1696f0b26b9c4c5002bd06c731b2
Alan Bushell <alanbushell91@gmail.com>
2023-01-14 14:47:28 +0000

    add s3 and static files connenction

---
+++ custom_storages.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from storages.backends.s3boto import S3Boto3Storage
+
+class StaticStorage(S3BotoStorage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3BotoStorage):
+    location = settings.MediaFILES_LOCATION
