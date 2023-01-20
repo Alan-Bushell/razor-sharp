710d5a8e79e4db797bf18425f4fd90e30d4c5fb7
Alan Bushell <alanbushell91@gmail.com>
2023-01-20 05:24:48 +0000

    fix previous commit bug causing server error

--- razor_sharp/urls.py
+++ razor_sharp/urls.py
@@ -22,16 +22,3 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'home.views.handle_404_error'
-
-
-
-
-
-urlpatterns = [
-    path('', include('myapp.urls')),
-    path('admin/', admin.site.urls),
-    # other url patterns go here...
-]
-
-handler404 = custom_404
-handler500 = custom_500
