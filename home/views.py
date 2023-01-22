82351d959f9dc707a70097cb071dbd034a83dc88
Alan Bushell <alanbushell91@gmail.com>
2023-01-22 07:48:39 +0000

    add privacy policy to website

--- home/views.py
+++ home/views.py
@@ -8,3 +8,9 @@ def index(request):
     """ A view to return index page """
 
     return render(request, 'home/index.html')
+
+
+def privacy(request):
+    """ A view to return the privacy policy page """
+
+    return render(request, 'home/privacy.html')
